ad38044f0ccf49c0011855174e2bf8a6ae0adcc5
ariard@student.42.fr <ariard@student.42.fr>
2016-11-13 17:16:39 +0100

    dimanche 17h15

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ariard <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/12 19:59:32 by ariard            #+#    #+#             */
-/*   Updated: 2016/11/13 15:28:17 by ariard           ###   ########.fr       */
+/*   Updated: 2016/11/13 16:40:38 by ariard           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,14 @@ int		main(int argc, char **argv)
 }
 
 
-//																		un tetra = 4 lignes 
-//																		si plus de 5 caracteres par ligne
-//																		si caracteres differents de # ou .
+//un tetra = 4 lignes 
+//si plus de 5 caracteres par ligne
+//si caracteres differents de # ou .
 //																		si plus de 5 #
-//si # ne se touche pas
+//																		si # ne se touche pas
 //																		si moins de 4#
-//																		saut de lignes entre chaque structure
-//																		si plus de 26 structures
+//saut de lignes entre chaque structure
+//si plus de 26 structures
 //
 //split verification dans fichier a part
 //verification descendante, a partir du premier verifier dans les x suivants si occurence jusqu au 4eme
