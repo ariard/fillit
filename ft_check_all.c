def1f4ca13e1ef7dedc87699b50290bac05e5899
ariard@student.42.fr <ariard@student.42.fr>
2016-11-14 15:18:46 +0100

    lundi 15h

--- ft_check_all.c
+++ ft_check_all.c
@@ -6,61 +6,53 @@
 /*   By: ariard <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/13 20:04:46 by ariard            #+#    #+#             */
-/*   Updated: 2016/11/13 20:20:45 by ariard           ###   ########.fr       */
+/*   Updated: 2016/11/14 15:18:02 by ariard           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
-#include <stdio.h>
-
-static int			ft_check_block_contact(size_t x, size_t xb, size_t y, size_t yb)
-{
-				if ((x + 1 == xb && y == yb) || (yb + 1 == yb && x == xb))
-				return (0);
-	return (1);			
-}
 
 static int			ft_check_pos_block(char *s, size_t x, size_t y, int block)
 {
-	size_t	xb;
-	size_t	yb;
-
-	xb = x;
-	yb = y;
-	while (yb % 5 == 0)
+	while (y % 5 != 0 && block < 5)
 	{
 		if (*s == 35)
-			if (ft_check_block_contact(x, y, xb, yb))	
+		{
+			if ((*(s + 1) == 35 || *(s + 5) == 35) ||
+				(*(s - 1) == 35 && *(s + 4) == 35))
 			{
 				block++;
-				x = xb;
-				y = yb;
 			}
-		if (xb == 5 && *s == 10)
+		}
+		if (x == 5 && *s == 10)
 		{
-			xb = 1;
-			yb++;
+			x = 1;
+			y++;
 			s++;
 		}
-		else (xb++ && *s++);
+		else if (*s++)
+			x++;
 	}
-	if (block != 1)
+	if (block != 4)
 		return (1);
 	return (0);
 }
 
-static int				ft_check_block(char *s, size_t x, size_t y)
+static int			ft_check_block(char *s, size_t x, size_t y)
 {
 	int				block;
 
 	block = 0;
-	while(*s)
+	while (*s)
 	{
 		if (*s == 35)
 			block++;
 		if (block == 1)
+		{
 			if (ft_check_pos_block(s, x, y, block))
 				return (1);
+			block++;
+		}
 		if (y % 5 == 0)
 			block = 0;
 		if ((x == 5 && *s == 10) || (y % 5 == 0 && *s == 10))
@@ -69,14 +61,15 @@ static int				ft_check_block(char *s, size_t x, size_t y)
 			y++;
 			s++;
 		}
-		else (x++ && *s++);
+		else if (*s++)
+			x++;
 	}
 	return (0);
 }
 
-static int				ft_check_files(char *s, size_t x, size_t y)
+static int			ft_check_files(char *s, size_t x, size_t y)
 {
-	if (*s != 10 && *s != 35 && *s != 46)		
+	if (*s != 10 && *s != 35 && *s != 46)
 		return (1);
 	if (x % 5 == 0 && *s != 10)
 		return (1);
@@ -84,7 +77,7 @@ static int				ft_check_files(char *s, size_t x, size_t y)
 		return (1);
 	if (y % 5 == 0 && ((*s != 10) || (*(s + 1) != 35 && *(s + 1) != 46)))
 		return (1);
-	return (0);	
+	return (0);
 }
 
 int					ft_check_all(char *s)
@@ -106,11 +99,12 @@ int					ft_check_all(char *s)
 			y++;
 			s++;
 		}
-		else (x++ && *s++);
+		else if (*s++)
+			x++;
 	}
 	x = 1;
 	y = 1;
-	if (ft_check_block(s, x, y))
+	if (ft_check_block(tmp, x, y))
 		return (1);
 	return (0);
 }
