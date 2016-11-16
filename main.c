65b19e6e8cf32524ef84a7726f0a6924fe9ff63c
ariard@student.42.fr <ariard@student.42.fr>
2016-11-16 14:56:23 +0100

    mercredi 15h

--- main.c
+++ main.c
@@ -20,11 +20,11 @@ int			main(int argc, char **argv)
 	i = 1;
 	s = ft_read_tetra(argv[1]);
 	tab = ft_generate(s);
-	while (i < ft_count_tetra(s))
-	{
-		printf("x1 : %d y1 : %d x2 : %d y2 : %d x3 : %d y3 : %d x4 : %d y4 : %d\n", tab[0]->shape[0].x, tab[0]->shape[0].y, tab[0]->shape[1].x, tab[0]->shape[1].y, tab[0]->shape[2].x, tab[0]->shape[2].y, tab[0]->shape[3].x, tab[0]->shape[3].y);
-		tab++;
-		i++;	
-	}
+//	while (i < ft_count_tetra(s))
+//	{
+//		printf("x1 : %d y1 : %d x2 : %d y2 : %d x3 : %d y3 : %d x4 : %d y4 : %d\n", tab[0]->shape[0].x, tab[0]->shape[0].y, tab[0]->shape[1].x, tab[0]->shape[1].y, tab[0]->shape[2].x, tab[0]->shape[2].y, tab[0]->shape[3].x, tab[0]->shape[3].y);
+//		tab++;
+//		i++;	
+//	}
 	return (0);
 }
